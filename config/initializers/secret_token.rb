39d6b5a94b3410536ba0aa044c71ef0cb12f826b
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-05-11 19:46:22 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeGirl::Application.config.secret_token = '2dcf69e01ac037123a5a3acad92d73f51dc9eb76111384c8438977a64de12d40812ceffc356155bee9155346b42f00121344b71821a9cda23caa2e8c736a740a'
