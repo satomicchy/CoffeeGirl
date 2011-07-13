63b06fa10e0fca16e28f12e356984bb1af14320a
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-07-14 01:10:51 +0900

    add strftime method in event#index,show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.order("date_on DESC").all
 
     respond_to do |format|
       format.html # index.html.erb
