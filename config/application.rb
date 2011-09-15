b6ce1a73cc7d5722415f7a5d9a8dc894e2e551e0
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-09-15 10:32:03 +0900

    set i18n ja.yml

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module CoffeeGirl
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     # Please note that JavaScript expansions are *ignored altogether* if the asset
     # pipeline is enabled (see config.assets.enabled below). Put your defaults in
