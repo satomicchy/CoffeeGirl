71cd6a68ec7caaf264fdfbc8a92b999e63e06bec
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-10-18 01:43:25 +0900

    ready to release on heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "smtp.gmail.com",
-  :user_name            => "",
-  :password             => "",
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
