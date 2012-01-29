a668dfd07dacbf4baf34cb6d873889ce030d7671
satomicchy <satomi_t_sora@yahoo.co.jp>
2012-01-29 15:40:09 +0900

    change list layout in report#index

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,7 @@ class ReportsController < ApplicationController
   # GET /reports
   # GET /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.all.group_by{|report| report.event}
 
     respond_to do |format|
       format.html # index.html.erb
